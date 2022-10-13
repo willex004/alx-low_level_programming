5ccab708607eefa324346f662e07f4c31b064580
Osas <oseikponmwosaosayande@gmail.com>
2022-10-13 13:59:51 -0700

    0x00-hello_world

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 /**
  * main - Entry
  * Return: Always 0
- **/
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
