235d6cd19bc2e911e6c14cfdbf491ae8611c956b
Osas <oseikponmwosaosayande@gmail.com>
2022-10-20 14:17:57 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print the numbers  0 - 9
+ * without 2 & 4
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int a = 0;
+
+	while (a < 10)
+	{
+		if (a != 2 && a != 4)
+			_putchar(a + '0');
+		a++;
+	}
+	_putchar('\n')
+}
