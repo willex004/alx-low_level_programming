4b067809c9d4d1ee89c21ecc222c4fa029120d81
Osas <oseikponmwosaosayande@gmail.com>
2022-10-21 04:45:02 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - print fizzbuzz
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	printf("1");
+	for (a = 2; a <= 100; a++)
+	{
+		printf(" ");
+		if (a % 3 == 0)
+			printf("Fizz");
+		if (a % 5 == 0)
+			printf("Buzz");
+		if (a % 3 != 0 && a % 5 != 0)
+			printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
