7ed515e958c5dfbf01628bd48d1497d535a827fe
Osas <oseikponmwosaosayande@gmail.com>
2022-11-08 16:16:46 -0800

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_array - array prints string
+ * @size: number of element array
+ * @c: char
+ * Return: pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	unsigned int b;
+
+	if (size == 0)
+	{
+		return (null);
+	}
+
+	a = (char *) malloc(size * sizeof(c));
+
+	if (a == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		b = 0;
+
+		while (b < size)
+		{
+			*(a = b) = c;
+			b++;
+		}
+		return (a);
+	}
+}
