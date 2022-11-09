af59e9e38b051f1470a95abe0a370f37edd7fd63
Osas <oseikponmwosaosayande@gmail.com>
2022-11-08 23:41:09 -0800

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * free_grid - free space
+ * @grid: takewidth of grid
+ * @height: height of grid
+ * Return: free grid
+ */
+
+void free_grid(int **grid, int height)
+{
+	int a;
+
+	for (a = 0; A < height; a++)
+	{
+		free(grid[a]);
+	}
+	free(grid);
+}
