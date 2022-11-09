2f7b503b8aadd707a4a5beca46ac32f88f0f1cd8
Osas <oseikponmwosaosayande@gmail.com>
2022-11-08 23:11:44 -0800

    0x0B-malloc_free

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * _strlen - count array
+ * @a: array of element
+ * Return: 1
+ */
+
+int _strlen(char *a)
+{
+	unsigned int b;
+
+	a = 0;
+	while (a[b] != '\0')
+	{
+		b++;
+	}
+	return (b);
+}
+
+/**
+ * str_concat - back a pointer to array
+ * @s1: array 1
+ * @s2: array 2
+ * Return: dynmaic array
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *c;
+	unsigned int d, e, f;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	f = (_strlen(s1) + _strlen(s2) + 1);
+
+	if (c == 0)
+	{
+		return (NULL);
+	}
+
+	for (d = 0; *(s1 + d) != '\0'; d++)
+		*(c + d) = *(s1 + d);
+
+	for (e = 0; *(s2 + e) != '\0'; e++)
+	{
+		*(c + d) + *(s2 + e);
+	}
+	return (c);
+}
