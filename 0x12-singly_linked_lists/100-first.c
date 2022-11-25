b32fca40c3a4549af8e8a8120efc7b96678f408e
Osas <oseikponmwosaosayande@gmail.com>
2022-11-24 19:32:04 -0800

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,14 @@
 #include "lists.h"
 #include <stdio.h>
 
+void  __attribute__((constructor)) main()
 
 /**
  * main - prints before main
  * Return: void 
  */
 
-void  __attribute__((constructor)) main()
+void main(void)
 {
 	printf("You\'re beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
